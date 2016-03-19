c0b5faf881ed04bbb285345e32548963e7221587
Raissa <raissaraahroo@gmail.com>
2016-03-19 12:18:17 -0300

    Create cabecalho.php

---
+++ cabecalho.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<title>WalMart- Bem Vindo!</title>
+			<link  href = "css/bootstrap.min.css"  rel= "stylesheet" >
+			<link  href = "css/walmart.css"  rel= "stylesheet" >
+		</head>
+
+		<body>
+				<nav class="navbar navbar-inverse">
+				  <div class="container-fluid">
+				    <!-- Brand and toggle get grouped for better mobile display -->
+				    <div class="navbar-header">
+				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+				      <a class="navbar-brand" href="index.php"><img class="img-responsive logo" src="img/logo-walmart.png"></a>
+				      <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
+				      <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span></a>
+				      <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
+				 </div>
+ 
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					     <ul class="nav navbar-nav">
+					        <li class="active"><a href="#">Seus pedidos</a></li>
+					        <li><a href="enderecos.php">Endereços Cadastrados</a></li>
+					       	<li><a href="#">Área do Cliente</a></li>
+					    </ul> 
+				      <form class="navbar-form navbar-left" role="search">
+				        <div class="form-group">
+				          <input type="text" class="form-control" placeholder="O que você procura?">
+				        </div>
+				        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+				      </form>
+				      
+				    </div>
+				  </div>
+				</nav>
