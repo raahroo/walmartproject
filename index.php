5c04a1f15c9d28218dd28edd72df1e174dfcb7ac
Raissa <raissaraahroo@gmail.com>
2016-03-19 12:16:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+	<?php 
+
+	require_once("cabecalho.php"); ?>
+
+	<div class="container-fluid" id="home">
+		<div class="row">
+			<div class="col-md-4">
+				<h1>Seus Pedidos</h1>
+			</div>
+
+			<a href="enderecos.php">
+				<div class="col-md-4">
+					<h1>Enderecos Cadastrados</h1>
+				</div>
+			</a>
+
+			<div class="col-md-4">
+				<h1>Area do Cliente</h1>
+			</div>
+	</div>
+
+
+	<div class="container-fluid" id="topo">
+		<p><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
+		Voltar ao topo</p>
+	</div>
+
+	</body>
+
+</html>
+
